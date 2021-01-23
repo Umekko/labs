8400bcb53e07f704eaed73a8be3a1a903d40ac28
Dean <dino.zavr1412@gmail.com>
2021-01-23 13:06:59 +0600

    first lesson

---
+++ 1_Урок/index.js
@@ -0,0 +1,78 @@
+// Числа
+let a = 2;
+let b = 3;
+let c = 21;
+
+let d = (b ** 2) - 4 * a * c;
+
+// console.log(d)
+
+
+
+//let num1 = 5;
+//let num2 = 7;
+//let num3 = 5;
+
+//let avg = (num1 + num2) / 2;
+
+//console.log(num1 ** 2);
+
+
+// Строки
+
+// let mailMessage = `
+//     Привет меня зовут: ${name + surname}
+
+//     name: 
+//     surname: Abramov
+//     мой номер: 
+// `;
+
+
+// let surname = "Abramov \n";
+// let message = `name: ${name} surname: ${surname}`;
+// console.log(name);
+
+// console.log(surname + " " + name);
+
+// let isHuman = false;
+// let isAdmin = true;
+
+// let userExists = false;
+
+// if (userExists == true) {
+//     console.log('Пользователь есть')
+// } else {
+//     console.log('Пользователя нет')
+// }
+
+// let zx;
+
+// console.log(zx);
+
+// let age = 50;
+
+// console.log(age >= 18)
+
+// if (age <= 18) {
+//     console.log('Вход запрещен')
+// } else if (age >= 50) {
+//     console.log('Нельзя')
+// } else {
+//     console.log('Добро пожаловать!')
+// }
+
+// let empty = null; // "", undefined, 0, NaN
+
+// console.log(10 / "azimbaev");
+// if (empty) {
+//     console.log('В переменной что то есть')
+// } else {
+//     console.log('В переменной ничего нет')
+// }
+
+console.log(typeof 1);
+console.log(typeof "");
+console.log(typeof typeof 2);
+
+
