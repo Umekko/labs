0cd83df3aa0ca7f02ed6aa282e890b606de95e97
Dean <dino.zavr1412@gmail.com>
2021-01-23 18:22:38 +0600

    lesson 2

---
+++ 3_УРОК/index.js
@@ -0,0 +1,81 @@
+// let user = {
+//    name: 'Dean',
+//    age: 22,
+//    isAdmin: true,
+//    'user-story': 'ldsa;ldsa;ldas;ld'
+// }
+
+// console.log(user.age);
+// console.log(user['user-story']);
+
+// Преобразование типов. Числа
+// let v1 = Number("11");
+// console.log(v1, typeof v1);
+// let v2 = Number(false);
+// console.log(v2, typeof v2);
+
+// let v3 = Number(true);
+// console.log(v3, typeof v3);
+
+// let v4 = Number('ksklkeq');
+// console.log(v4, typeof v4);
+
+// console.log(typeof null);
+
+// console.log("2" * 25);
+// console.log(10 * false);
+// console.log(10 * null);
+
+// console.log(50 / "2");
+
+// console.log(50 / false);
+
+// console.log(String(false));
+// console.log(String(null));
+// console.log(String(NaN));
+// console.log(String(52));
+
+// console.log(false + "1");
+
+// console.log(Boolean(''));
+// console.log(Boolean(' '));
+// console.log(Boolean(null));
+// console.log(Boolean(null + 1));
+
+// console.log(5 > 1);
+// console.log(5 < 10);
+// console.log(5 >= 5);
+// console.log(5 <= 5);
+
+// Не строгое сравнение
+// console.log(5 == '5');
+
+// Строгое сравнение
+// console.log(5 === '5');
+
+// console.log(5 !== '5');
+
+// console.log('я' > 'а');
+
+// let age = 20;
+// let male = 'man';
+
+// let canAccess = (age > 18) && (male === 'man');
+
+// if (canAccess) {
+//     console.log('Welcome');
+// }
+
+// if (age < 18 || male != 'man') {
+//     console.log('Good bye');
+// }
+
+const TRUE_PASSWORD = '12345678';
+const TRUE_NAME = 'Nyatin';
+
+const user = {
+    name: 'Nyatin',
+    isAdmin: true,
+    password: '12345678'
+};
+
