a425609e593da1590ae52f7379cc73e70feed4da
Dean <dino.zavr1412@gmail.com>
2021-01-26 20:40:18 +0600

    For, while, do while loops

---
+++ 4_Урок_циклы/index.js
@@ -0,0 +1,84 @@
+// let counter = 0;
+// counter++; //постфикс
+// ++counter; //префикс
+// console.log(counter++);
+// console.log(--counter);
+
+// let views = 0;
+// console.log(++views);
+// console.log(views);
+
+// let seconds = 60;
+// console.log(seconds--);
+// console.log(seconds);
+
+// let views = 0;
+// views = views + 1;
+// views++;
+// console.log(views);
+
+// let views = 10 % 3;
+// console.log(views);
+
+// Преобразование в число с помощью унарного плюса
+// const a = "1";
+// const b = "2";
+
+// console.log(+a + +b);
+
+// let a = 1;
+// let b = 2;
+// let c = 3;
+
+// let a = 1, b = 2, c = a + b;
+// console.log(a, b, c);
+
+// let bagPrice = 300;
+// let shoesPrice = 1000;
+
+// let orderPrice = bagPrice + shoesPrice;
+
+// orderPrice = orderPrice - ((orderPrice / 100) * 30); 
+
+// console.log(orderPrice);
+
+// let counter = 0;
+
+// while (counter < 100) {
+//     counter++;
+//     if (counter % 3 === 0 && counter % 5 === 0) {
+//         console.log('ping');
+//     } else if (counter % 5 === 0) {
+//         console.log('pong')
+//     } else if (counter % 3 === 0) {
+//         console.log('ping pong');
+//     } else {
+//         console.log(counter);
+//     }
+// }
+
+// let counter = 0;
+// do { 
+//     counter++;
+//     if (counter % 3 === 0 && counter % 5 ===0) {
+//         console.log('ping pong');
+//     } else if (counter % 5 === 0) {
+//         console.log('pong');
+//     } else if (console % 3 === 0) {
+//         console.log('ping')
+//     } else {
+//         console.log(counter);
+//     }
+// } while(counter < 100);
+
+// for (let i = 0; i < 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('Ping Pong');
+//     } else if (i % 5 === 0) {
+//         console.log('Pong');
+//     } else if (i % 3 === 0) {
+//         console.log('Ping');
+//     } else {
+//         console.log(i);
+//     }
+// }
