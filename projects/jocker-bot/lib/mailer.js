4c6ed7fb5185816411045f0ea4ae3123fafb8b4b
Dean <dino.zavr1412@gmail.com>
2021-02-27 18:23:56 +0600

    mailing

--- projects/jocker-bot/lib/mailer.js
+++ projects/jocker-bot/lib/mailer.js
@@ -4,22 +4,27 @@ const message = `
     Привет это nodejs
 `;
 
+const mailForMailing = 'neolabs.mailer@gmail.com';
+const passwordForMail = 'Ctujlyzctynz,hm';
+
 const transport = nodemailer.createTransport({
 	service: 'gmail',
 	secure: false,
 	port: 25,
 	auth: {
-		user: 'dino.zavr1412',
-		pass: 'password',
+		user: mailForMailing,
+		pass: passwordForMail,
 	},
 	tls: {
 		rejectUnauthorized: false,
 	},
 });
 
+console.log('Отправляю письмо');
+
 transport
 	.sendMail({
-		from: 'dino.zavr1412@gmail.com',
+		from: mailForMailing,
 		to: 'dino.zavr1412@gmail.com',
 		text: message,
 		subject: 'Тестовое письмо',
@@ -30,3 +35,5 @@ transport
 	.catch((e) => {
 		console.log('error', e.message);
 	});
+
+console.log('Синхронная операция');
