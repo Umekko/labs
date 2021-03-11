05ce39919e31639eca55755dcecc9c9e4c13a1ba
Dean <dino.zavr1412@gmail.com>
2021-03-12 00:03:11 +0600

    nodejs rest api example

--- projects/news-app/index.js
+++ projects/news-app/index.js
@@ -1,35 +1,56 @@
 const express = require('express');
-
 const app = express();
+const bodyParser = require('body-parser');
 
-const PORT = 5000;
+app.use(bodyParser.json());
 
-const news = [
-    {
-        title: 'В кр прошли выборы',
-        content: 'бджывдждвыфждвыфж дыдвждвыфждвыфждвжыфд',
-        date: new Date(),
-    }
+const PORT = 8181; // 3000-8666
+
+const DATABASE_NEWS = [
+	{
+		title: '11 апреля будут выборы',
+		content: 'С Ж пояснил за конституцию',
+		date: new Date(),
+		lang: 'ru',
+	},
+	{
+		title: '11 aprelya budut vibory',
+		content: 'Sadyr Sadyr !!!',
+		date: new Date(),
+		lang: 'en',
+	},
 ];
 
-// endpoint /heroes 
-// GET -> [{ name: 'Captain america' }]
+app.get('/news', async (request, response) => {
+	try {
+		const lang = request.headers.language; // ru
 
-app.get('/heroes', async (req, res) => {
-    
-})
+		const fiteredNews = DATABASE_NEWS.filter((n) => {
+			return n.lang === lang;
+		});
 
-app.get('/news', async (req, res) => {
-    try {
-        res.status(200).json({
-            message: 'Новости за последние 12 часов',
-            news: news
-        })
-    } catch (e){
-        console.log(e);
+		response.status(200).json(fiteredNews);
+	} catch (e) {
+		response.status(500).json(e.message);
+	}
+});
+
+app.post('/create-news', async (req, res) => {
+	try {
+        const news = req.body;
+        news.date = new Date();
+
+        DATABASE_NEWS.push(news);
+
+        res.status(201).json({
+            message: 'Новость создана',
+            allNews: DATABASE_NEWS
+        });
+	} catch (e) {
+
     }
 });
 
 app.listen(PORT, () => {
-    console.log('Backend запущен!! на порту: ' + PORT);
+	console.log(`Приложение запущено на ${PORT} порту`);
 });
